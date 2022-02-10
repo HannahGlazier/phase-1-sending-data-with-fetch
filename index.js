ba69217ec6d22ec1d7450c449c7d99c83402f75b
Hannah Glazier <hrayglazier@aol.com>
2022-02-10 12:39:55 -0700

    commit tree d87133e92fe7fa94cf9d3e1c91e2aafcc3a55a77

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const URL = 'http://localhost:3000/users'
+
+
+function submitData( name, email ) {
+    return fetch( URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then(resp => resp.json())
+      .then( function (respObj) {
+        document.body.innerHTML = respObj[ "id" ]
+      } )
+      .catch(function(error) {
+        document.body.innerHTML = error.message
+      } )
+  }
